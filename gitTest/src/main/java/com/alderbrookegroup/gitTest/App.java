8e46049294a6b22dc663b17fcdcade448fc713ce
kitodevteam <32455690+kitodevteam@users.noreply.github.com>
2017-10-12 16:53:51 +0100

    Update App.java

--- gitTest/src/main/java/com/alderbrookegroup/gitTest/App.java
+++ gitTest/src/main/java/com/alderbrookegroup/gitTest/App.java
@@ -11,7 +11,7 @@ public class App {
 	
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World!!!";
+		return "Hello Docker World :)";
 	}
 
 	public static void main(String[] args) {
